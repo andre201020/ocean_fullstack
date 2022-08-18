e70993db521879edacfd53247af72eae9c9da996
name fulanom <email@email.com>
2022-08-18 09:28:54 -0300

    aula 3

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # Aula 1
 1. Baixar e instalar o [Visual Studio Code](https://code.visualstudio.com/download)
 2. Baixar e instalar o [NodeJS e NPM](https://nodejs.org/en/download)
-3. Abrir um terminal, criar pasta do projeto e adentrá-la: `mkdir [app_name]
-cd [app_name]`
+3. Abrir um terminal, criar o diretório-raiz do projeto e adentrá-lo: `mkdir [PastaProjeto]
+cd [PastaProjeto]`
 4. Abrir a pasta da aplicação no vscode: `code .`
-5. No terminal, executar comando: `npm init` 'respondendo' as questões.Fazer com que o `entry point: ` aponte para o **arquivo de entrada da aplicação/Maexpin file**, exemplo `index.js`.
+5. No terminal, executar comando: `npm init` 'respondendo' as questões. Fazer com que o `entry point: ` aponte para o **arquivo de entrada da aplicação/Maexpin file**, exemplo `index.js`.
 6. Instalar o **express**: `npm install express` ou 
 7. No vscode, criar o **main file** da aplicação: por exemplo, `index.js` e incluir o código de exemplo [Hello World](https://expressjs.com/en/starter/hello-world.html)
 8. executar o **main file** no nodejs: `node index.js`
 9. teste no navegador: <localhost:3000>
 
 # Aula 2
+1. instalar o **nodemon**: `npm install --save-dev nodemon`.
+**nodemon** reinicializa aplicação nodejs sempre que seu código fonte altera. 
+2. no terminal, executar a aplicação:
+1. `npm run dev` reiniciar execução da aplicação nodejs assim que seu código fonte for alterado
+2. `npm run dev` (ou `node .\index.js`) execução da aplicação segue, indiferente a alteração no fonte
+3. no vscode, utilizando a extensão 'Thunder Client', é possível gerar `requests` **restfull**, para ir testando a aplicação
 
-1. De dentro da pasta da aplicação, './backend', instalar o nodemom: `npm install --save-dev nodemon`
-2. De dentro da pasta `./backend` executar a aplicação com `npm run dev` (ou `node .\index.js` que não reinicia automaticamente qdo o código fonte é alterado) 
-3. no vscode, utilizando a extensão 'Thunder Client', gerar `requests` para ir testando a aplicação
+# Aula 3
+1. instalar o **mongodb**: `npm install mongodb`.
+2. abrir `mongoDB compass`, criar banco de dados uma coleção e incluir alguns documentos.
+3. alterar `index.js` para lidar com o BD
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+const dbName = "jornada-fullstack-agosto-22";
+
+//declarar função main()
+async function main() {
+//realizar a conexão o MongoClent
+//MongoClent -> MongoDAtabase -> MongoCollection
+
+// Conexões com o client podem levar um tempo para efetivarem. 
+//Portanto, utilizamos o mecanismo de Promises do JavaScript, 
+//que permite aguardar esse tempo.
+//Para isso, vamos usar o async/await
+
+console.log(`Tentando conexão com o DB...`);
+
+const client = await MongoClient.connect(url);
+const db = client.db(dbName);
+const collection = db.collection("pontuacoes");
+
+console.log(`Conectado com sucesso ao DB!`);
+
 const app = express();
 const port = 3000;
 
+let rota;
+
+//sinalizamos para o express que stamos usando JSON no body das requisições
+app.use(express.json());
+
 app.get('/', (req, res) => {
+  rota = '/';
+  console.log(`Rota: ${rota} - [GET]`);
   res.send('Rellou Vuorldi!');
 });
 
 app.get('/oi', function (req, res) {
-  res.send('Oi mundão vei de guerra, cada vez mais dominado por homis marvados e covardes!! E aqui no Brazil-Brazuca, cada vez mais estes homis estão mais armados... covardes malditos  ');
+  rota = '/oi';
+  console.log(`Rota: ${rota} - [GET]`);
+  res.send('Brazil-Brazuca dos homi marvado e armado!! Mardita mardição de marvado armado!! ');
 });
 
 //sistema de ranking - criar lista de pontuações
@@ -24,31 +56,47 @@ const lista = [
     pontos: 5,
   },
   {
-    id:1,
+    id:3,
     nome: "Dedáxi",
     pontos: 6,
   },
 ];
 
 //Endpoint READ ALL - [GET] / pontuacoes
-app.get('/pontuacoes', function (req, res) {
-  res.send(lista);
+app.get('/pontuacoes', async function (req, res) {
+  const itens = await collection
+  .find()
+  .sort({pontos: -1}) //ordenação: - '-1' por ordem decrescente; - '+1' por ordem crescente; 
+  .limit(10) //retorna até 10 documentos/registros
+  .toArray();
+  rota = `/pontuacoes`;
+  console.log(`Rota: ${rota} - [GET]`);
+  //res.send(lista);
+  res.send(itens);
+
 });
 
 //Endpoint READ ALL - [GET] / pontuacoes
-app.post("/pontuacoes", function (req, res) {
+app.post("/pontuacoes", async function (req, res) {
   const item = req.body;
-  console.log(item);
-  res.send("cria pontuação");
-});
+  //console.log(item);
+  //res.send("cria pontuação");
 
-/*lista.push({
-  id: lista.length + 1,
-  nome: item.nome,
+//adicionar o item na lista
+//lista.push({
+//  id: lista.length + 1,
+//  nome: item.nome,
+//  pontos: item.pontos,
 
+  await collection.insertOne(item);
+  res.send(item);
 });
-*/
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`ouvindo na porta: ${port}`);
 });
+
+}
+
+//executar a função main()
+main();
